5a7eb1d4574fa6c680be792af595b2d85435a004
mhaslinsky <mhaslinsky@gmail.com>
2022-12-05 19:15:36 -0500

    init commit

---
+++ pages/api/posts.js
@@ -0,0 +1,29 @@
+import { prisma } from "../../server/db/client";
+
+function titleFromCode(code) {
+  //remove whitespace split on newline, take first line, replace comment marker with empty string
+  //gives title of post from first line comment
+  return code.trim().split("\n")[0].replace("// ", "");
+}
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  switch (method) {
+    case "POST":
+      const { language, code } = req.body;
+      const title = titleFromCode(code);
+      const post = await prisma.post.create({
+        data: {
+          title,
+          language,
+          code,
+        },
+      });
+      res.status(201).json(post);
+      break;
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
